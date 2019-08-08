699b5f0e8191ab027bf87fdd58a777c3f6928b0f
Niranjan <nirpanig@publicisgroupe.net>
2019-08-08 11:24:17 +0530

    test

--- src/main/java/com/kafka/KafkaResource.java
+++ src/main/java/com/kafka/KafkaResource.java
@@ -2,6 +2,9 @@ package com.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +28,22 @@ public class KafkaResource {
 
 	@GetMapping("publish/name/{name}/message/{message}")
 	public String post(@PathVariable("name") String name, @PathVariable("message") String message) {
-		kafkaJsonTemplate.send("testjson", new User(name, message));
+		ListenableFuture<SendResult<String, User>> future = kafkaJsonTemplate.send("testjson", new User(name, message));
+		future.addCallback(new ListenableFutureCallback<SendResult<String, User>>() {
+
+			@Override
+			public void onSuccess(SendResult<String, User> result) {
+				System.out.println(result.getProducerRecord());
+				System.out.println(result.getRecordMetadata().offset());
+				System.out.println(result.getRecordMetadata().partition());
+				System.out.println(result.getRecordMetadata().topic());
+			}
+
+			@Override
+			public void onFailure(Throwable ex) {
+				System.out.println(ex.getMessage());
+			}
+		});
 		return "published successfully";
 	}
 
